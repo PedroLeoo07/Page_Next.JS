1f99bdf94cf1937b55b7bda277dccc832dabc9a2
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2025-03-13 08:56:41 -0300

    style: change in texts and stylization

--- pages/conteudo.jsx
+++ pages/conteudo.jsx
@@ -10,12 +10,23 @@ export default function Conteudo() {
 
     return (
         <div className={styles.conteudo}>
-            <h1>Conteúdo</h1>
-            <p>Este é um exemplo de conteúdo. Vamos adicionar mais algumas informações para tornar esta página mais interessante.</p>
+            <h1 className={styles.title}>Sobre Mim</h1>
+            <p className={styles.p}>O meu nome é Leonardo Oliveira atualmente tenho 17 anos, atualmente
+            estou estudando na rede Sesi e Senai, na rede Sesi estou no 
+            meu último ano e com grandes expectativas pro futuro, no 
+            Senai estou estudando técnico em análise de desenvolvimento de sistemas...
+            futuramente pretendo seguir a área de desenvolvimento e conseguir várias oportunidades
+            </p>
             <ul>
-                <li>Informação interessante 1</li>
-                <li>Informação interessante 2</li>
-                <li>Informação interessante 3</li>
+                <h3 className={styles.title}>Interesses</h3>
+                <p className={styles.p}>Gosto muito de Ler(especificamente livros cristãos)</p>
+                <p className={styles.p}>Amo ouvir música e tocar também</p>
+                <p className={styles.p}>Gosto de jogar alguns jogos de mundo aberto</p>
+            </ul>
+            <ul>
+                <h3 className={styles.title}>Gostos</h3>
+                <p className={styles.p}>No meu tempo livre gosto bastante de treinar</p>
+                <p className={styles.p}>Eu gosto bastante de escrever poemas</p>
             </ul>
             <button className={styles.button} onClick={goBack}>Voltar</button>
         </div>
